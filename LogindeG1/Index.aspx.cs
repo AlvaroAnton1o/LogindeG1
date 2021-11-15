ddeea25fb82941533da835f22907a9063867d1ee
Samuel Isaí Mejia Alfaro <USIS935921@ugb.edu.sv>
2021-11-15 16:48:30 -0400

    Agregar archivos de proyecto.

---
+++ LogindeG1/Index.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LogindeG1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["usuariologueado"] != null)
+            {
+                string usuariologueado = Session["usuariologueado"].ToString();
+                lblBienvenida.Text = "Bienvenido/a " + usuariologueado;
+            }
+            else
+            {
+                Response.Redirect("Login_G1.aspx");
+            }
+        }
+        protected void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Session.Remove("usuariologueado");
+            Response.Redirect("Login_G1.aspx");
+        }
+    }
+}
---
+++ LogindeG1/Registro.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace LogindeG1
+{
+    public partial class Registro : System.Web.UI.Page
+    {
+      
+    }
+}
